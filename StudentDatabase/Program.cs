46346766f9ef347f89da8ba8f199713d28576026
Andrew <andrewpullen1@gmail.com>
2023-01-27 14:54:11 -0500

    init commit

---
+++ StudentDatabase/Program.cs
@@ -0,0 +1,165 @@
+﻿using StudentDatabase;
+
+string[] name = new string[]
+{
+    "Peter Parker",
+    "Bruce Wayne",
+    "Ash Williams",
+    "Laurie Strode",
+    "Sidney Prescott",
+    "Snake Plissken",
+    "Alex Murphy",
+    "Henry Armitage",
+    "Rick Deckard",
+    "Joeseph Dredd"
+};
+
+string[] hometown = new string[]
+{
+    "New York City",
+    "Gotham",
+    "Elk Grove",
+    "Haddonfield",
+    "Woodsboro",
+    "Cleveland",
+    "Detroit",
+    "Arkham",
+    "Los Angeles",
+    "Mega-City One"
+};
+
+string[] favoriteFood = new string[]
+{
+    "Pizza",
+    "Nachos",
+    "Churros",
+    "Pumpkin Pie",
+    "Popcorn",
+    "Beef Jerky",
+    "Coney Dog",
+    "Roast Beef",
+    "Noodles",
+    "Recycled Food"
+};
+
+Console.Write("Welcome! ");
+
+
+bool loop = true;
+do
+{
+    string answer = null;
+    bool isValidAnswer = false;
+
+    Console.Write("Which student would you like to know more about?\n" +
+        $"Enter a number 1-{name.Length}: ");
+
+    string userStudent = Console.ReadLine();
+
+    if (int.TryParse(userStudent, out int student))
+    {
+        bool isValidStudent = StudentDatabaseClass.IsValidStudent(student, name.Length);
+
+        if (isValidStudent == true) 
+        {
+            int index = student - 1;
+            string category;
+            bool isValidCategory = false;
+            do
+            {
+                Console.Write($"\nStudent {student} is {name[index]}. What would you Like to know?\n" +
+                    $"Enter \"hometown\" or \"favorite food\": ");
+                category = Console.ReadLine().ToLower().Trim();
+
+                isValidCategory = StudentDatabaseClass.IsValidCategory(category);
+
+                if (isValidCategory == false)
+                {
+                    Console.WriteLine("\nI'm sorry, that is not a valid answer.\n");
+                }
+            }while (isValidCategory == false);
+
+            
+            if (category == "home" || category == "town" || category == "hometown")
+            {
+                Console.WriteLine($"\n{name[index]} is from {hometown[index]}\n");
+            }
+            else
+            {
+                Console.WriteLine($"\n{name[index]}'s favorite food is {favoriteFood[index]}\n");
+            }
+
+            do
+            {
+                Console.Write("Would you like to know about another student?\n" +
+                        "Enter \"y\" for yes or \"n\" for no: ");
+
+                answer = Console.ReadLine().ToLower().Trim();
+
+                isValidAnswer = StudentDatabaseClass.isValidAnswer(answer);
+
+                if (isValidAnswer == false)
+                {
+                    Console.WriteLine("\nI'm sorry, that is not a valid response.\n");
+                }
+
+            } while (isValidAnswer == false);
+
+            if (answer == "y")
+            {
+                Console.WriteLine("\nOkay.\n");
+            }
+            else
+            {
+                Console.WriteLine("\nOkay. Thanks for using our student database.\n" +
+                    "Have a great day!");
+
+                loop = false;
+            }
+        }
+        else
+        {
+            //Console.WriteLine("\nI'm Sorry, that is not a valid selection\n");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("\nI'm sorry, that is not a valid selection\n");
+        Console.WriteLine("test");
+    }
+
+    if (loop == true)
+    {
+        do
+        {
+            Console.Write("Would you like to see a list of our students?\n" +
+                "Enter \"y\" for yes or \"n\" for no: ");
+
+            answer = Console.ReadLine();
+
+            isValidAnswer = StudentDatabaseClass.isValidAnswer(answer);
+
+            if (isValidAnswer == false)
+            {
+                Console.WriteLine("\nI'm sorry, that is not a valid response.\n");
+            }
+
+        } while (isValidAnswer == false);
+
+        if (answer == "y")
+        {
+            Console.WriteLine();
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} {name[i]}");
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }
+
+} while (loop == true);
---
+++ StudentDatabase/StudentDatabaseClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    public static class StudentDatabaseClass
+    {
+        //Student number validation
+        public static bool IsValidStudent(int student, int nameArray)
+        {
+            if (student > 0 && student < nameArray + 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Category validation
+        public static bool IsValidCategory(string category)
+        {
+            if (category == "home" || category == "town" || category == "hometown" ||
+                category == "food" || category == "favorite" || category == "favorite food")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Answer Validation
+        public static bool isValidAnswer(string answer)
+        {
+            if (answer == "y" || answer == "n")
+            {
+                return true;
+            }
+            return false;
+        }
+            
+    }
+}
